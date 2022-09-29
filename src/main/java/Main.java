49d69bbf4cd07f94e2c685c57a201a38165c415a
akonstantinov <akonstantinov@gear-games.com>
2022-09-29 04:13:04 +0300

    Добавил использование тредпула для тестирования работы

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,13 @@
 import controller.GameplayController;
 
+import java.util.concurrent.Executors;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
-            new Thread(new GameplayController()).start();
+        var pool = Executors.newFixedThreadPool(2000);
+        for (int i = 0; i < 1500; i++) {
+            pool.submit(new GameplayController());
         }
+        pool.shutdown();
     }
 }
